21ed2d1adc58b89e37a4bf669b3c7b8e2a794b6b
jabobon1 <jabobon@yandex.ru>
2020-03-28 13:48:45 +0300

    change base template

--- app/api/home.py
+++ app/api/home.py
@@ -7,4 +7,8 @@ blueprint = Blueprint('home', __name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
